96b822bbb5159eed4fcaf7954efc240fc94abe5f
Max Riveiro <kavu13@gmail.com>
2019-06-01 12:23:09 +0300

    Fix test script, replace npm with Yarn

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-npm i --save-dev -q "webpack@$WEBPACK_VERSION" && \
+yarn add --dev "webpack@$WEBPACK_VERSION" && \
   node_modules/.bin/webpack --config "test/webpack-$WEBPACK_VERSION.config.js"
